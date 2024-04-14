4d5017ad9a1aeae6d1e6df0a0abc046a37361b39
Iosdhilipan <dhilipan.r@inovark.com>
2024-04-14 15:03:59 +0530

    Add two numbers

--- POC problem Leet code.playground/Contents.swift
+++ POC problem Leet code.playground/Contents.swift
@@ -21,3 +21,39 @@ func twoSum (_ input:[Int],_ target:Int)-> [Int]{
 }
 print(twoSum([2,7,11,15], 9))
 
+
+
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+
+
+func concatenateNumbe (_ input:[Int])-> Int{
+    var  concatenateNumbe = 0
+    
+    for num in input.reversed() {
+    concatenateNumbe =  concatenateNumbe * 10
+    concatenateNumbe =  concatenateNumbe + num
+        
+    }
+    return concatenateNumbe
+}
+
+func reverseNum (_ input:Int)->[Int]{
+let inputString = String(input)
+    var output = [Int]()
+    for char in inputString.reversed() {
+        if let char = Int(String(char)){
+            output.append(char)
+        }
+       
+    }
+    return output
+}
+
+let l1 = concatenateNumbe([2,4,3])
+let l2 = concatenateNumbe([5,6,4])
+
+
+
+print(reverseNum(l1 + l2))  // Ans : [7,0,8]
